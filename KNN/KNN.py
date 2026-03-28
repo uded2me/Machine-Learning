f6eeb25b05004c3ede054a39896fb106e72e016f
Alexander Nguyen <alexandernguyen533@gmail.com>
2026-03-28 01:12:27 -0700

    Added and read CSV file

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -2,3 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+df = pd.read_csv('500hits.csv', encoding = 'latin-1') #build DataFrame/ fix encoding issues
+print(df.head()) #prints first 5 player data
+
+df = df.drop(columns= ['PLAYER', 'CS']) #drop uneeded data
+
