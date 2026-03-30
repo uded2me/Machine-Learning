d91b7ee02b392f4a7f7df30b67f921619e59c752
Alexander Nguyen <32279195+uded2me@users.noreply.github.com>
2026-03-29 21:19:58 -0700

    Updated KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -7,7 +7,26 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
 df = pd.read_csv('500hits.csv', encoding = 'latin-1') #build DataFrame/ fix encoding issues
-print(df.head()) #prints first 5 player data
+#print(df.head()) #prints first 5 player data
 
 df = df.drop(columns= ['PLAYER', 'CS']) #drop uneeded data
 
+X = df.iloc[:,0:13]
+
+y = df.iloc[:,13]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state= 11, test_size=0.2)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+
+X_train = scaler.fit_transform(X_train)
+
+X_test = scaler.fit_transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=8)
+
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+print(y_pred)
