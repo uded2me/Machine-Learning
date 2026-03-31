4a1a5b2226d73d455da40cb6baf39f3ccf9b5e43
Alexander Nguyen <alexandernguyen533@gmail.com>
2026-03-30 21:15:08 -0700

    Finished KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -9,24 +9,38 @@ from sklearn.metrics import classification_report
 df = pd.read_csv('500hits.csv', encoding = 'latin-1') #build DataFrame/ fix encoding issues
 #print(df.head()) #prints first 5 player data
 
-df = df.drop(columns= ['PLAYER', 'CS']) #drop uneeded data
+df = df.drop(columns= ['PLAYER', 'CS']) #drop uneeded columns
 
-X = df.iloc[:,0:13]
+X = df.iloc[:,0:13] 
 
-y = df.iloc[:,13]
+y = df.iloc[:,13] #y will be based on boolean in column 13 aka if they made it into the Hall of Fame or not
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, random_state= 11, test_size=0.2)
 
+#apply scaler to model to normalize data magnitude
 scaler = MinMaxScaler(feature_range=(0,1))
 
 X_train = scaler.fit_transform(X_train)
 
 X_test = scaler.fit_transform(X_test)
 
+#different num of neighbors will result in different outputs for the model adjust accordingly
 knn = KNeighborsClassifier(n_neighbors=8)
 
 knn.fit(X_train, y_train)
 
-y_pred = knn.predict(X_test)
+#this will output either 1 or 0 boolean for if the stats make it into the Hall of Fame
+y_pred = knn.predict(X_test) 
 
-print(y_pred)
+#print(y_pred)
+
+#provide accuracy of the model
+print(knn.score(X_test, y_test))
+
+#Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+#Classification Report
+cr = classification_report(y_test, y_pred)
+print(cr)
